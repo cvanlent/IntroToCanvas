0745be110c4df7a26892394e4b064b521ec0ed4d
Colleen van Lent <cvanlent@users.noreply.github.com>
2019-01-16 13:53:14 -0500

    Add files via upload

--- js/animate.js
+++ js/animate.js
@@ -17,6 +17,7 @@ function myMove() {
 function randomColor(){
   //Now do funky math to change the color
       var color = '#'; // 
-            var letters = ['000000','FF0000','00FF00','0000FF','FFFF00','00FFFF','FF00FF','C0C0C0']; //Set your colors here
-                color += letters[Math.floor(Math.random() * letters.length)];
+      var letters = ['000000','FF0000','00FF00','0000FF','FFFF00','00FFFF','FF00FF','C0C0C0']; //Set your colors here
+      color += letters[Math.floor(Math.random() * letters.length)];
+      return color;
 }
